be63d8c9e3b63cc6cf870f10b99ce7e28e9373c1
maxandron <ron.suhodrev@gmail.com>
2018-11-19 02:25:02 +0200

    Initial attempts to write memory

--- src/linuxMain/kotlin/suspender/suspender.kt
+++ src/linuxMain/kotlin/suspender/suspender.kt
@@ -1,18 +1,20 @@
 package suspender
 
 import kotlinx.cinterop.*
+import platform.linux.Elf64_WordVar
 import platform.posix.*
 
-const val DEFAULT_PID = "3555"
+const val DEFAULT_PID = "6983"
 const val DEFAULT_DUMP = "/tmp/suspender.dump"
 const val DEFAULT_BINARY = "/home/maxandron/work/CTFs/digimon/digimon_nowait"
+const val CURRENT_MODE = "load"
 
 fun main(args_temp: Array<String>) {
-    val args = arrayOf("program", "load")
+    val args = arrayOf("program", CURRENT_MODE)
 
     if (args.size != 2) {
-        println("Usage: ./suspender suspend")
-        println("Usage: ./suspender load")
+        println("Usage:\t./suspender suspend")
+        println("\t\t./suspender load")
         exit(1)
     }
 
@@ -23,31 +25,79 @@ fun main(args_temp: Array<String>) {
     }
 }
 
-fun loadProgram(dumpFile: String, binaryPath: String) {
+fun loadProgram(dumpFileName: String, binaryPath: String) {
     val pid = fork()
     when (pid) {
         0 -> {
             // Child
             memScoped {
-                ptrace(PTRACE_TRACEME, 0, nativeNullPtr, nativeNullPtr)
-//                exec(binaryPath.cstr.ptr.rawValue) // will automagically get a SIGTRAP signal
+                ptrace(PTRACE_TRACEME, 0, 0, 0)
+                execv(binaryPath, cValuesOf<ByteVar>()) // will automagically get a SIGTRAP signal
             }
         }
         -1 -> exit(1) // Error
         else -> {
             // Parent
-//            wait(nativeNullPtr)
-            val checkpointFile = fopen("/tmp/test", "w") ?: return
-            loadSegments(checkpointFile)
+            waitpid(-1, cValuesOf(0), 0)
+            val dumpFile = fopen(dumpFileName, "rb") ?: return
+            try {
+//                ptrace(PTRACE_ATTACH, pid, 0, 0)
+                loadSegments(dumpFile, pid)
+            } finally {
+                // Restore the process
+//                ptrace(PTRACE_DETACH, pid, 0, 0)
+                kill(pid, SIGCONT)
+                fclose(dumpFile)
+            }
         }
     }
 }
 
-fun loadSegments(checkpointFile: CPointer<FILE>) {
+fun loadSegments(dumpFile: CPointer<FILE>, pid: Int) {
     memScoped {
-        val buffer = allocArray<ByteVar>(1)
-        // Read header size
-        fread(buffer, ByteVar.size.convert(), 1.convert(), checkpointFile)
+        while (true) {
+            val buffer = alloc<LongVar>()
+
+            var amountRead = fread(buffer.ptr, LongVar.size.convert(), 1.convert(), dumpFile)
+            val segmentStart = buffer.value
+            println("Segment start = $segmentStart")
+            if (amountRead != 1.convert<size_t>()) break
+
+            amountRead = fread(buffer.ptr, LongVar.size.convert(), 1.convert(), dumpFile)
+            val segmentEnd = buffer.value
+            println("Segment end = $segmentEnd")
+            if (amountRead != 1.convert<size_t>()) break
+
+            val segmentSize = segmentEnd - segmentStart
+            val dataBuffer = allocArray<Elf64_WordVar>(segmentSize)
+            amountRead = fread(dataBuffer, Elf64_WordVar.size.convert(), segmentSize.convert(), dumpFile)
+            if (amountRead != (segmentEnd - segmentStart).convert<size_t>()) break
+
+            loadSegment(pid, segmentStart, segmentEnd, dataBuffer)
+        }
+    }
+}
+
+fun loadSegment(pid: Int, segmentStart: Long, segmentEnd: Long, data: CArrayPointer<Elf64_WordVar>) {
+//    val segmentSize = segmentEnd - segmentStart
+//    val memoryFile = fopen("/proc/$pid/mem", "rb") ?: return
+//
+//    try {
+//        fseek(memoryFile, segmentStart, 0)
+//        val amountWrote = fwrite(data, ByteVar.size.convert(), segmentSize.convert(), memoryFile)
+//        println("Wrote $amountWrote / $segmentSize")
+//        if (amountWrote != segmentSize.convert<size_t>()) {
+//            println("Failed writing segment $segmentStart:$segmentEnd")
+//        }
+//    } finally {
+//        fclose(memoryFile)
+//    }
+
+    for (address in segmentStart..segmentEnd step Elf64_WordVar.size) {
+        val testPtrace = ptrace(PTRACE_POKEDATA, pid, address, data[address - segmentStart])
+        if (-1L == testPtrace) {
+            println("Failed writing at $address")
+        }
     }
 }
 
@@ -58,7 +108,7 @@ fun suspendProgram(pid: Int, dumpFileName: String) {
 
     val mappings = extractMappings(pid)
 
-    val dumpFile = fopen(dumpFileName, "w") ?: return
+    val dumpFile = fopen(dumpFileName, "wb") ?: return
     val memoryFile = fopen("/proc/$pid/mem", "rb") ?: return
 
     try {
@@ -94,17 +144,19 @@ fun saveSegment(
 
     println("saving segment from $startAddress to $endAddress ($segmentSize)")
 
-    // Write header (<startAddress> <endAddress>, <segmentSize>)
-    fwrite(cValuesOf(startAddress), LongVar.size.convert(), 1.convert(), dumpFile)
-    fwrite(cValuesOf(endAddress), LongVar.size.convert(), 1.convert(), dumpFile)
-    fwrite(cValuesOf(segmentSize), LongVar.size.convert(), 1.convert(), dumpFile)
-
     memScoped {
-        val memoryBuffer = allocArray<ByteVar>(segmentSize)
+        val memoryBuffer = allocArray<Elf64_WordVar>(segmentSize)
         fseek(memoryFile, startAddress, SEEK_SET)
-        val amountRead = fread(memoryBuffer, ByteVar.size.convert(), segmentSize.convert(), memoryFile)
+        val amountRead = fread(memoryBuffer, Elf64_WordVar.size.convert(), segmentSize.convert(), memoryFile)
         println("Read $amountRead bytes")
-        fwrite(memoryBuffer, ByteVar.size.convert(), segmentSize.convert(), dumpFile)
+        if (amountRead.toLong() != segmentSize) {
+            println("skipping section")
+            return
+        }
+
+        fwrite(cValuesOf(startAddress), LongVar.size.convert(), 1.convert(), dumpFile)
+        fwrite(cValuesOf(endAddress), LongVar.size.convert(), 1.convert(), dumpFile)
+        fwrite(memoryBuffer, Elf64_WordVar.size.convert(), segmentSize.convert(), dumpFile)
     }
 }
 
